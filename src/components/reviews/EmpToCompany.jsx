04b7df8536a83ff4158e123d18241ece1dd9c438
nadgire <abhinadgire@hotmail.com>
2025-03-20 10:34:13 +0530

    overlapping div problem solved

--- src/components/reviews/EmpToCompany.jsx
+++ src/components/reviews/EmpToCompany.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const EmpToCompany = () => {
   return (
-    <div>
-      <h2 className='text-2xl font-semibold'>Submit the review</h2>
+    <div className='w-full'>
+      <h2 className='text-2xl font-semibold mx-10'>Submit the review</h2>
       <div className='m-10'>
         <form action="" className='flex flex-col gap-3'>
-          <select name="" id="" className='w-1/5 border rounded-sm py-1'>
+          <select name="" id="" className='w-fit border rounded-sm py-1 px-5'>
             <option value="" selected disabled>Select Company</option>
             <option value="">IBM</option>
             <option value="">TSYS</option>
             <option value="">TCS</option>
           </select>
-          <textarea name="" id="" className='border p-1 rounded-sm w-1/2 min-h-56 max-h-56' placeholder='Enter your review here..' ></textarea>
+          <textarea name="" id="" className='border p-1 rounded-sm w-2/3 min-h-56 max-h-56' placeholder='Enter your review here..' ></textarea>
           <div className='flex gap-5 items-center'>
             <span>Ratings out of 10</span> 
             <input type="range" />
           </div>
-          <input type="submit" className='border rounded-sm w-1/8 py-1' />
+          <input type="submit" className='border rounded-sm w-fit px-10  py-1' />
         </form>
       </div>
     </div>
