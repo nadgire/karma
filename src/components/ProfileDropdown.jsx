2e25d08a8325fda2dc318f676ece87e8be4e8e54
nadgire <abhinadgire@hotmail.com>
2025-03-20 10:32:40 +0530

    Links added

--- src/components/ProfileDropdown.jsx
+++ src/components/ProfileDropdown.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router'
 import { TbLogout } from "react-icons/tb";
 import { BiEditAlt } from "react-icons/bi";
 import { ImProfile } from "react-icons/im";
 
 
-const ProfileDropdown = () => {
+const ProfileDropdown = (props) => {
+    function clickToClose() {
+        props.flag=false;
+    }
     return (
         <div className='absolute right-0 top-20 w-60 h-auto p-5 bg-blue-200 rounded-b-lg'>
             <ul className='space-y-5 py-5'>
-                <li className='flex items-center gap-2'>
-                    <ImProfile className='text-2xl' />
-                    Profile
+                <li>
+                    <Link to="/dashboard/profile" className='flex items-center gap-2' onClick={clickToClose}>
+                        <ImProfile className='text-2xl' />
+                        Profile
+                    </Link>
                 </li>
                 <li className='flex items-center gap-2'>
-                    <BiEditAlt className='text-2xl' />
-                    Change Password
+                    <Link to="/dashboard/change-password" className='flex items-center gap-2' onClick={clickToClose}>
+                        <BiEditAlt className='text-2xl' />
+                        Change Password
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/" className='flex items-center gap-2'>
+                    <Link to="/" className='flex items-center gap-2' onClick={clickToClose}>
                         <TbLogout className='text-2xl' />
                         Logout
                     </Link>
